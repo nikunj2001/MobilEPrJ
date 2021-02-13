4b57597912b63b9915874e25285d147500fd20eb
nikunj2001 <nikunj.gupta_cs19@gla.ac.in>
2021-02-14 00:32:38 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const hamburger = document.querySelector('.hamburger');
+const navList = document.querySelector('.nav-list');
+hamburger.addEventListener('click',()=>{
+    navList.classList.toggle('show');
+});
